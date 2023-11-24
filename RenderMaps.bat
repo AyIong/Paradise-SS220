c76eb514d9c609b0d911f984c9c5af045ea9105e
Aylong <alexanderkitsa@gmail.com>
2023-11-24 09:55:19 +0200

    RenderMaps

---
+++ RenderMaps.bat
@@ -0,0 +1,4 @@
+dmm-tools minimap _maps/map_files220/cyberiad/cyberiad.dmm
+dmm-tools minimap _maps/map_files220/delta/delta.dmm
+dmm-tools minimap _maps/map_files220/MetaStation/MetaStation.dmm
+dmm-tools minimap _maps/map_files220/generic/CentComm.dmm
